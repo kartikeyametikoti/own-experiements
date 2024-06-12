de35ecb38ba7d3f5fee513ae00bb9c191c31f195
kartikeya-1112 <metikotikartikeya1112@gmail.com>
2024-06-12 23:31:59 +0530

    new one2

--- kartikeya.java
+++ kartikeya.java
@@ -4,8 +4,8 @@ public class kartikeya
     public static void main(String args[])
     {
 Scanner s=new Scanner(System.in);
-        int a;
-        a=s.nextInt();
+if (s.hasNextInt()) {
+    int a = scanner.nextInt();
         if(a==10)
         {
             System.out.println("yes it is 10");
